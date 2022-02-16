9223091589e977bcfe833a1c81b6f83882e6304c
Mark Cabanero <mark.cabanero@clever.com>
2022-02-16 11:08:04 -0800

    feat: Replace 'Learn React' link with Moment date

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import logo from './logo.svg';
 import './App.css';
+import Moment from 'react-moment';
+import 'moment-timezone';
 
 function App() {
+  const dateToFormat = '1977-04-19T12:59-0500';
   return (
     <div className="App">
       <header className="App-header">
@@ -9,14 +12,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Moment>{dateToFormat}</Moment>
       </header>
     </div>
   );
